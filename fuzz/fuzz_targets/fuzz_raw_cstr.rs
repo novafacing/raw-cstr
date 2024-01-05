266f1ad57f63c13e2e6012b587f4e7df3c9ff3f4
novafacing <rowanbhart@gmail.com>
2024-01-05 14:05:23 -0800

    Cleanup

--- fuzz/fuzz_targets/fuzz_raw_cstr.rs
+++ fuzz/fuzz_targets/fuzz_raw_cstr.rs
@@ -1,6 +1,3 @@
-// Copyright (C) 2023 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
 #![no_main]
 #![forbid(unsafe_code)]
 
--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,3 @@
-// Copyright (C) 2023 Intel Corporation
-// SPDX-License-Identifier: Apache-2.0
-
 use std::ffi::CStr;
 
 use anyhow::Result;
